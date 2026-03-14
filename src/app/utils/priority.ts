54a2822b66c6afc8ff068b17d5bdfac6d20d8708
Cesar Gabriel <cesargabrielsousasousa196@gmail.com>
2026-03-14 20:35:03 -0300

    feat: Initialize core database schema, introduce messaging, and enhance complaint priority with new fields, spatial indexing, and a debug query.

--- src/app/utils/priority.ts
+++ src/app/utils/priority.ts
@@ -10,12 +10,13 @@
 // }
 
 export function getPriorityColor(priority: number) {
+
     const clamped = Math.max(0, Math.min(priority, 10))
 
-    // const normalized = Math.pow(clamped / 10, 1.5)
-    // const hue = 120 - normalized * 120
-    
-    const hue = 120 - (clamped / 10) * 120
+    const normalized = Math.pow(clamped / 10, 1.5)
+    const hue = 120 - normalized * 120
+
+    // const hue = 120 - (clamped / 10) * 120
 
     return `hsl(${hue}, 100%, 45%)`
 }
